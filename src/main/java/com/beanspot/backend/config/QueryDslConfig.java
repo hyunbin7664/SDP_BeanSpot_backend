88733d0b8fa73f9759741b788fbc662ad620cbbe
HaSam <ssam8610@naver.com>
2026-01-10 14:17:43 +0900

    feat: QueryDSL 설정 추가

---
+++ src/main/java/com/beanspot/backend/config/QueryDslConfig.java
@@ -0,0 +1,21 @@
+package com.beanspot.backend.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaRepositories("com.beanspot.backend.repository")
+public class QueryDslConfig {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(){
+        return new JPAQueryFactory(entityManager);
+    }
+}
